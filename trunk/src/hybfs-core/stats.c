3f0eedf1f9ae054552595c2e5169908e87e51815
shteph <shteph@179ac568-995c-11dd-87e9-a30ea5446288>
2008-11-08 21:25:59 +0000

    hybfs: added option support. The mount point now can be a directory different from root. Also, added parsing for multiple directories(branches).

--- trunk/src/hybfs-core/stats.c
+++ trunk/src/hybfs-core/stats.c
@@ -3,6 +3,10 @@
  
  Copyright (C) 2008-2009  Stefania Costache, Dan Pintilei
 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
  */
 
 #include <sys/time.h>
@@ -19,10 +23,15 @@
 int hybfs_getattr(const char *path, struct stat *stbuf)
 {
 	int res;
+	char p[PATHLEN_MAX];
 
-	res = lstat(path, stbuf);
+	DBG_SHOWFC();
+	dbg_print("path: %s\n",path);
+
+	resolve_path(path,p,PATHLEN_MAX);
+	res = lstat(p, stbuf);
 	if (res == -1)
-		return -errno;
+	return -errno;
 
 	return 0;
 }
@@ -30,10 +39,14 @@ int hybfs_getattr(const char *path, struct stat *stbuf)
 int hybfs_access(const char *path, int mask)
 {
 	int res;
+	char p[PATHLEN_MAX];
 
-	res = access(path, mask);
+	DBG_SHOWFC();
+
+	resolve_path(path,p,PATHLEN_MAX);
+	res = access(p, mask);
 	if (res == -1)
-		return -errno;
+	return -errno;
 
 	return 0;
 }
@@ -43,6 +56,8 @@ int hybfs_utimens(const char *path, const struct timespec ts[2])
 	int res;
 	struct timeval tv[2];
 
+DBG_SHOWFC	();
+
 	tv[0].tv_sec = ts[0].tv_sec;
 	tv[0].tv_usec = ts[0].tv_nsec / 1000;
 	tv[1].tv_sec = ts[1].tv_sec;
@@ -50,7 +65,7 @@ int hybfs_utimens(const char *path, const struct timespec ts[2])
 
 	res = utimes(path, tv);
 	if (res == -1)
-		return -errno;
+	return -errno;
 
 	return 0;
 }
@@ -58,10 +73,14 @@ int hybfs_utimens(const char *path, const struct timespec ts[2])
 int hybfs_statfs(const char *path, struct statvfs *stbuf)
 {
 	int res;
+	char p[PATHLEN_MAX];
 
-	res = statvfs(path, stbuf);
+	DBG_SHOWFC();
+
+	resolve_path(path,p,PATHLEN_MAX);
+	res = statvfs(p, stbuf);
 	if (res == -1)
-		return -errno;
+	return -errno;
 
 	return 0;
 }
